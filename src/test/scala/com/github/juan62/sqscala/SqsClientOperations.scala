8ef83c8a2e652189eea9c2b2346670769043d605
juan62 <okuda@socket.co.jp>
2015-10-08 20:04:48 +0900

    # add: delete queue spec

--- src/test/scala/com/github/juan62/sqscala/SqsClientOperations.scala
+++ src/test/scala/com/github/juan62/sqscala/SqsClientOperations.scala
@@ -1,5 +1,6 @@
 package com.github.juan62.sqscala
 
+import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
 import org.specs2.specification.BeforeAfterAll
 import org.specs2.specification.core.Env
@@ -7,19 +8,23 @@ import org.specs2.specification.core.Env
 import scala.util.Try
 
 class SqsClientOperations(specEnv: Env) extends Specification with ElasticMqContext with BeforeAfterAll {
-  implicit val ee = specEnv.executionEnv
-  implicit val ec = specEnv.executionContext
-
   var client: SqsClient = _
 
   override def beforeAll(): Unit = client = ConfiguredSqsClient()
 
   override def afterAll(): Unit = client.shutdown()
 
-  "create and get queue then delete" in {
-    val queueName = QueueName("test")
+  val unit = ()
+
+  "create queue by client.queue method" in { implicit ee: ExecutionEnv =>
+    val queueName = QueueName("test1")
     Try(client.queue(queueName, createIfNotExists = true)) must beSuccessfulTry
-    client.deleteQueue(queueName) must be_==(()).await
+    client.deleteQueue(queueName) must be_==(unit).await
   }
 
+  "create queue by client.createQueue method" in { implicit ee: ExecutionEnv =>
+    val queueName = QueueName("test2")
+    client.createQueue(queueName) must be_==(unit).await
+    client.deleteQueue(queueName) must be_==(unit).await
+  }
 }
