643f952242b15cc66677e748c16b63086d5ae150
yoskhdia <okuda@socket.co.jp>
2016-01-04 14:25:58 +0900

    #mod: performance issue - skip synchronized block

--- src/main/scala/com/github/yoskhdia/sqscala/SqsClient.scala
+++ src/main/scala/com/github/yoskhdia/sqscala/SqsClient.scala
@@ -9,29 +9,34 @@ import com.amazonaws.services.sqs.{AmazonSQSAsync, AmazonSQSAsyncClient}
 import scala.concurrent.{Future, Promise}
 
 trait SqsClient {
+  import SqsClient._
 
   def awsClient: AmazonSQSAsync
 
-  protected def getQueueUrl(queueName: QueueName, createIfNotExists: Boolean): String = synchronized {
-    try {
-      val request = new GetQueueUrlRequest(queueName.simplified)
+  protected def getQueueUrl(queueName: QueueName, createIfNotExists: Boolean): String = {
+    val request = new GetQueueUrlRequest(queueName.simplified)
+    if (createIfNotExists) {
+      synchronized {
+        try {
+          awsClient.getQueueUrl(request).getQueueUrl
+        } catch {
+          case e: QueueDoesNotExistException =>
+            awsClient.createQueue(new CreateQueueRequest(queueName.simplified)).getQueueUrl
+          case e: Throwable => throw e
+        }
+      }
+    } else {
+      // skip synchronized block if you don't need create queue
       awsClient.getQueueUrl(request).getQueueUrl
-    } catch {
-      case e: QueueDoesNotExistException if createIfNotExists =>
-        awsClient.createQueue(new CreateQueueRequest(queueName.simplified)).getQueueUrl
-      case e: Throwable => throw e
     }
   }
 
   def queue(queueName: QueueName, createIfNotExists: Boolean = false): SqsQueue = {
-    val outer = this
-    new SqsQueue {
-      override val name: QueueName = queueName
+    queue(queueName, getQueueUrl(queueName, createIfNotExists))
+  }
 
-      override protected val queueUrl: String = getQueueUrl(queueName, createIfNotExists)
-
-      override protected val client: SqsClient = outer
-    }
+  def queue(queueName: QueueName, queueUrl: String): SqsQueue = {
+    new SqsQueueImpl(queueName, queueUrl, this)
   }
 
   def deleteQueue(queueName: QueueName): Future[Unit] = {
@@ -65,23 +70,23 @@ trait SqsClient {
 
 object SqsClient {
   /**
-   * apply sqs client.
-   *
-   * @param credentialProvider AWS credentials provider.
-   *                           Default AWS credentials provider chain that looks for credentials in this order:
-   *                           <ul>
-   *                           <li>Environment Variables -
-   *                           <code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code>
-   *                           (RECOMMENDED since they are recognized by all the AWS SDKs and CLI except for .NET),
-   *                           or <code>AWS_ACCESS_KEY</code> and <code>AWS_SECRET_KEY</code> (only recognized by Java SDK)
-   *                           </li>
-   *                           <li>Java System Properties - aws.accessKeyId and aws.secretKey</li>
-   *                           <li>Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs and the AWS CLI</li>
-   *                           <li>Instance profile credentials delivered through the Amazon EC2 metadata service</li>
-   *                           </ul>
-   * @param regions AWS regions.
-   * @return SQS client
-   */
+    * apply sqs client.
+    *
+    * @param credentialProvider AWS credentials provider.
+    *                           Default AWS credentials provider chain that looks for credentials in this order:
+    *                           <ul>
+    *                           <li>Environment Variables -
+    *                           <code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code>
+    *                           (RECOMMENDED since they are recognized by all the AWS SDKs and CLI except for .NET),
+    *                           or <code>AWS_ACCESS_KEY</code> and <code>AWS_SECRET_KEY</code> (only recognized by Java SDK)
+    *                           </li>
+    *                           <li>Java System Properties - aws.accessKeyId and aws.secretKey</li>
+    *                           <li>Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs and the AWS CLI</li>
+    *                           <li>Instance profile credentials delivered through the Amazon EC2 metadata service</li>
+    *                           </ul>
+    * @param regions AWS regions.
+    * @return SQS client
+    */
   def apply(credentialProvider: AWSCredentialsProvider = new DefaultAWSCredentialsProviderChain(),
             regions: Regions = Regions.DEFAULT_REGION): SqsClient = {
     val awsClient = new AmazonSQSAsyncClient(credentialProvider)
@@ -90,9 +95,11 @@ object SqsClient {
   }
 
   def apply(awsClient: AmazonSQSAsync): SqsClient = {
-    val ac = awsClient
-    new SqsClient {
-      override val awsClient: AmazonSQSAsync = ac
-    }
+    new SqsClientImpl(awsClient)
   }
+
+  private class SqsClientImpl(val awsClient: AmazonSQSAsync) extends SqsClient
+
+  private class SqsQueueImpl(val name: QueueName, protected val queueUrl: String, protected val client: SqsClient) extends SqsQueue
+
 }
