41480f3897ad06c0f6b8f43df849210dbaaee141
yoskhdia <okuda@socket.co.jp>
2016-11-06 12:09:57 +0900

    mod: for do not use implicit conversions

--- src/main/scala/com/github/yoskhdia/sqscala/package.scala
+++ src/main/scala/com/github/yoskhdia/sqscala/package.scala
@@ -7,39 +7,104 @@ import com.github.yoskhdia.sqscala.serializer.StringSerializer
 
 package object sqscala {
 
-  implicit val stringToMessageAttributeValue =
-    (s: String) => new MessageAttributeValue().withDataType("String").withStringValue(s)
-  implicit val intToMessageAttributeValue =
-    (i: Int) => new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
-  implicit val shortToMessageAttributeValue =
-    (i: Short) => new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
-  implicit val longToMessageAttributeValue =
-    (i: Long) => new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
-  implicit val floatToMessageAttributeValue =
-    (i: Float) => new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
-  implicit val doubleToMessageAttributeValue =
-    (i: Double) => new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
-  implicit val binaryToMessageAttributeValue =
-    (b: Array[Byte]) => new MessageAttributeValue().withDataType("Binary").withBinaryValue(ByteBuffer.wrap(b))
-  implicit val byteBufferToMessageAttributeValue =
-    (b: ByteBuffer) => new MessageAttributeValue().withDataType("Binary").withBinaryValue(b)
+  implicit class StringToMessageAttributeValue(val s: String) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("String").withStringValue(s)
+    }
+  }
 
-  implicit val messageAttributeValueToString =
-    (v: MessageAttributeValue) => v.getStringValue
-  implicit val messageAttributeValueToInt =
-    (v: MessageAttributeValue) => v.getStringValue.toInt
-  implicit val messageAttributeValueToShort =
-    (v: MessageAttributeValue) => v.getStringValue.toShort
-  implicit val messageAttributeValueToLong =
-    (v: MessageAttributeValue) => v.getStringValue.toLong
-  implicit val messageAttributeValueToFloat =
-    (v: MessageAttributeValue) => v.getStringValue.toFloat
-  implicit val messageAttributeValueToDouble =
-    (v: MessageAttributeValue) => v.getStringValue.toDouble
-  implicit val messageAttributeValueToBinary =
-    (v: MessageAttributeValue) => v.getBinaryValue.array()
-  implicit val messageAttributeValueToByteBuffer =
-    (v: MessageAttributeValue) => v.getBinaryValue
+  implicit class IntToMessageAttributeValue(val i: Int) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
+    }
+  }
+
+  implicit class ShortToMessageAttributeValue(val i: Short) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
+    }
+  }
+
+  implicit class LongToMessageAttributeValue(val i: Long) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
+    }
+  }
+
+  implicit class FloatToMessageAttributeValue(val i: Float) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
+    }
+  }
+
+  implicit class DoubleToMessageAttributeValue(val i: Double) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Number").withStringValue(i.toString)
+    }
+  }
+
+  implicit class BinaryToMessageAttributeValue(val b: Array[Byte]) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Binary").withBinaryValue(ByteBuffer.wrap(b))
+    }
+  }
+
+  implicit class ByteBufferToMessageAttributeValue(val b: ByteBuffer) extends AnyVal {
+    def toMessage: MessageAttributeValue = {
+      new MessageAttributeValue().withDataType("Binary").withBinaryValue(b)
+    }
+  }
+
+
+  implicit class MessageAttributeValueToScalaObject(val v: MessageAttributeValue) extends AnyVal {
+    private def validate[T](dataType: String)(f: MessageAttributeValue => T): T = {
+      if (v.getDataType == dataType) {
+        f(v)
+      } else {
+        throw new Exception
+      }
+    }
+
+    // String Data Type
+
+    def toStringValue: String = validate("String") { mv =>
+      mv.getStringValue
+    }
+
+    // Number Data Type
+
+    private def numberValue: String = validate("Number") { mv =>
+      val s = mv.getStringValue
+      if (s == null) {
+        throw new Exception("For the Number data type, you must use StringValue.")
+      }
+      s
+    }
+
+    def toInt: Int = numberValue.toInt
+
+    def toShort: Short = numberValue.toShort
+
+    def toLong: Long = numberValue.toLong
+
+    def toFloat: Float = numberValue.toFloat
+
+    def toDouble: Double = numberValue.toDouble
+
+    // Binary Data Type
+
+    def toBinary: Array[Byte] = validate("Binary") { mv =>
+      val b = mv.getBinaryValue
+      if (b == null) {
+        return Array.emptyByteArray
+      }
+      b.array()
+    }
+
+    def toByteBuffer: ByteBuffer = validate("Binary") { mv =>
+      mv.getBinaryValue
+    }
+  }
 
 
   // NOT implemented in AWS
